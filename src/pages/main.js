c46e708f3c64dfec14b6a3700c915bf26ac7ce21
William Bryan <wwbryan00@gmail.com>
2021-02-22 22:38:23 -0800

    add a character json file

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "../components/card";
 import Hero from "../components/hero";
 import navHeader from "../components/navHeader";
 
-function Main() {
-    return (
-        <div className="main">
-            <navHeader />
-            <Hero />
-            
-        </div>
-    );
+class Main extends Component {
+    
+    state = {
+        image: "",
+        chosen: false,
+        characters: [""]
+    }
+
+
+
+
+    render() {
+        return (
+            <div className="main">
+                <navHeader />
+                <Hero />
+                {this.state.characters.map((res) => (
+                    <Card image={res} />
+                ))}
+            </div>
+        );
+    }
+
 }
