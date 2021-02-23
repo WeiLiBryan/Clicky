e24af817789b0f2730d9a9b3e5455bb35a2a6e1f
William Bryan <wwbryan00@gmail.com>
2021-02-22 22:14:58 -0800

    link app.js to main.js and edit index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import Page from "./pages/main.js";
+
+
 function App() {
-  return (
-    
-  );
+  return <Page />
 }
 
 export default App;
---
+++ src/pages/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../components/card";
+import Hero from "../components/hero";
+import navHeader from "../components/navHeader";
+
+function Main() {
+    return (
+        <div className="main">
+            <navHeader />
+            <Hero />
+            
+        </div>
+    );
+}
