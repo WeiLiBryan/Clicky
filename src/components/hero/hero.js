9e5a3d2ef39fe3e20396508b5657f0029a9cecd8
William Bryan <wwbryan00@gmail.com>
2021-02-22 21:58:29 -0800

    make hero image for page

---
+++ src/components/hero/hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hero() {
+    return <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1>Clicky</h1>
+            <p>Test your memory by clicking the a picture that hasn't been clicked yet!</p>
+        </div>
+    </div>
+}
+
+export default Hero;
